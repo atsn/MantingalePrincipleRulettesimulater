19fb39b2d268d68aaae5c988e6d253e5d07c8022
Nikolaj Dittmann Nielsen <niko2514@edu.easj.dk>
2016-08-29 11:22:53 +0200

    Jeg har indtastet de enkelte metoder. samt properties. De virker ikk endnu.

--- MantingalePrincipleRulettesimulater/Gambler.cs
+++ MantingalePrincipleRulettesimulater/Gambler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,46 @@ namespace MantingalePrincipleRulettesimulater
 {
     class Gambler
     {
+
+        private int Cash { get; set; }
+
+        //constructor
+
+        public Gambler 
+        {
+
+
+        }
+
+            //metode betcash
+
+        public void betCash()
+        {
+            
+
+
+        }
+
+        //metode GambleMoney
+
+        public void GambleMoney()
+        {
+            
+        }
+
+        //metode recievecash
+
+        public void recievecash()
+        {
+          
+
+            
+        }
+
+        
+
+
     }
+
+
 }
