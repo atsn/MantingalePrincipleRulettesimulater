89c57b0915d6fcbb245b191b6983f9d014647e1d
anders <anders_Stubberup@hotmail.com>
2016-08-29 11:22:26 +0200

    Har lavet noget på RuleteController

--- MantingalePrincipleRulettesimulater/RulleteController.cs
+++ MantingalePrincipleRulettesimulater/RulleteController.cs
@@ -8,5 +8,37 @@ namespace MantingalePrincipleRulettesimulater
 {
     class RulleteController
     {
+        private bool betOnEven { get; set; }
+        private bool BenOnOdd { get; set; }
+        private bool RouletteNumber { get; set; }
+
+
+        public int getrouletenumber() { int RouletteNumber = 0; return RouletteNumber; }
+
+
+
+
+        public bool betEven(bool isbettingeven)
+        {
+
+            int RouletteNumber = getrouletenumber();
+
+            if (IsOdd(RouletteNumber))
+            {
+                return true;
+            }
+
+            else return false;
+        }
+        
+
+
+        private static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
     }
+
+
 }
+
